5fe11bc963b6e8431298a196aad9a76346f4b103
adrianBrands <adrianbrandshaug@hotmail.com>
2022-09-19 20:56:00 +0200

    creatings posts using the POST method for testing

--- js/script.js
+++ js/script.js
@@ -14,3 +14,22 @@ fetch('https://nf-api.onrender.com/api/v1/social/auth/login', {
     
     
     const accsessToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MTcwLCJuYW1lIjoiYWRyaWFuX2IiLCJlbWFpbCI6IkFkckJyYTEwMDY2QHN0dWQubm9yb2ZmLm5vIiwiYXZhdGFyIjpudWxsLCJiYW5uZXIiOm51bGwsImlhdCI6MTY2MzU5NjM2MX0.mTrix3JDspZF6hoPFfMUZHLZN4oZSwRAiInJplQngK8";
+
+    /*fetch('https://nf-api.onrender.com/api/v1/social/posts', {
+        method: 'POST', 
+        body: JSON.stringify({
+            title: "Test",
+            body: "testest" 
+        }), 
+        headers: {
+            'content-type': 'application/json; charset=UTF-8',
+            'Authorization': `Bearer ${accsessToken}`
+        },
+    })
+
+    .then((response) => response.json())
+    .then((json) => console.log(json));*/
+
+
+
+    
