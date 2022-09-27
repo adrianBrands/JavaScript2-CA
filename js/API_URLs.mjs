1bda5e53441f07753c24ed0d403bd09b896bded3
adrianBrands <adrianbrandshaug@hotmail.com>
2022-09-27 18:03:03 +0200

    workin on retriving posts from the API

--- js/API_URLs.mjs
+++ js/API_URLs.mjs
@@ -3,6 +3,6 @@
  const baseUrl_API = "https://nf-api.onrender.com"; 
  const registerUrl = `${baseUrl_API}/api/v1/social/auth/register`;
  const signinrUrl = `${baseUrl_API}/api/v1/social/auth/login`;
- const posts = `${baseUrl_API}/api/v1/social/posts?_author=true`;
+ const posts = `${baseUrl_API}/api/v1/social/posts/?_author=true`;
 
 export {signinrUrl, registerUrl, posts, authorization};
--- js/post_feed.mjs
+++ js/post_feed.mjs
@@ -1,6 +1,6 @@
 import { posts, authorization } from "./API_URLs.mjs";
 
-    
+const displaySomething = document.querySelector(".col-sm-7");    
     
 async function getPosts(url) {
     try{
@@ -15,6 +15,47 @@ async function getPosts(url) {
         const response = await fetch(url, getData);
         console.log(response);
         const json = await response.json();
+        for (let i = 0; i < json.length; i++){
+            console.log(json[i]);
+            displaySomething.innerHTML += `<div class="card mb-3">
+            <img src="${json[i].media}" class="card-img-top" alt="black and white picture of a huge cliff by the ocean">
+            <div class="card-body">
+                <h5 class="card-title">${json[i].author.name}</h5>
+                <p class="card-text"><small class="text-muted">Published ${json[i].updated}</small></p>
+            </div>
+        </div>`;
+
+        }
+       
+    } catch (error) {
+       console.log(error);
+    }
+}   
+
+getPosts(posts);
+
+
+//add posts to the feed
+
+/*async function makePost(post) {
+    try{
+        const postData = {
+            method: "POST",
+            body: JSON.stringify({
+                title: "testAbra",
+                body: "helloAbra",
+                tas:["string_test"]
+
+            }),
+            headers: {
+                "content-type": "application/json; charset=UTF-8",
+                Authorization: `Bearer ${authorization}`,
+            },
+            
+        };
+        const response = await fetch(post, postData);
+        console.log(response);
+        const json = await response.json();
         console.log(json);
         
         
@@ -24,10 +65,8 @@ async function getPosts(url) {
     } catch (error) {
        console.log(error);
     }
-}   
+}*/
 
-getPosts(posts);
+//makePost(posts);
 
-
-//make post
-
+//427
