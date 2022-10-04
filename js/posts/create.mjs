d915c7209985420be02973e20d67f22c13aae50e
adrianBrands <adrianbrandshaug@hotmail.com>
2022-10-04 22:24:55 +0200

    wokrin on creating a post from the form inputs

--- js/posts/create.mjs
+++ js/posts/create.mjs
@@ -11,9 +11,52 @@ async function createPost(postData) {
     });
 
     const result = await response.json();
+    
 
     console.log(result);
 }
+
+
+
+
+function createFormListener() {
+    const form = document.querySelector("#create_post");
+
+
+    if (form) {
+        form.addEventListener("submit", (event) => {
+            event.preventDefault();
+            const form = event.target;
+            const formData = new FormData(form);
+            const post = Object.fromEntries(formData.entries())
+
+            console.log(post);
+            
+            
+    
+            createPost(post);
+        } );
+    
+    }  
+   
+
+   
+   
+    
+}
+
+createFormListener();
+
+
+
+//delete id 906, 913, 915
+    
+
+
+
+   
+ 
+  
 
 
 /*createPost({
